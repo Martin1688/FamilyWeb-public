93733da75278d5cd58185e5c7f4ac30a0c96e3db
martin <da5407@gmail.com>
2022-11-08 15:04:48 +0800

    moved server site to cyclic cloud debug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'https://good-lime-squid-cape.cyclic.app/',
+  apiBaseUrl: 'https://good-lime-squid-cape.cyclic.app/api',
   production: true,
   siteKey: '6Lc1mhQhAAAAAAvBU2oxo4-Qqmn_LnlTpUhoqJiF',
 };
